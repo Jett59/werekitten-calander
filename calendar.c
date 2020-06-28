90bf8d40dab8bd1e6cd36ff81a3c081fc4e046a2
Jett Thompson <jett.thompson.59@gmail.com>
2020-06-28 11:11:47 +1000

    added a dictionary for the program to read and print to the console

--- calendar.c
+++ calendar.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-
+int main(int argc, char* argv[]) {
+FILE* dictionary;
+if(argc > 1) {
+dictionary = fopen(argv[1], "r");
+}else {
+dictionary = fopen("dictionary.txt", "r");
+}
+if(dictionary == 0) {
+fprintf(stderr, "error: unable to open dictionary file\n");
+return 1;
+}
+printf("reading: \n");
+char str[] = "hello!\0";
+while(fgets(str, 30, dictionary) != 0) {
+printf("%s ", str);
+}
+return 0;
 }
